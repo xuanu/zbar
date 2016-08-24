e6fc8b36b192dc98b308a59d18f1e0ab5da53c32
zeffect <625417632@qq.com>
2016-08-24 12:22:04 +0800

    修改界面，无法完成从相册选取

---
+++ qr_zbar/src/main/java/cn/zeffect/qr_zbar/utils/ZbarUtils.java
@@ -0,0 +1,24 @@
+package cn.zeffect.qr_zbar.utils;
+
+import android.content.ContentUris;
+import android.content.Context;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by zeffect on 2016/8/23.
+ */
+public class ZbarUtils {
+}
